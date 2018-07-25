31a8070bdf48bf08712c5691c33b68aa25924e77
James Fenn <18jafenn90@gmail.com>
2018-07-25 21:54:17 +0100

    basic category displaying stuff

--- js/things.js
+++ js/things.js
@@ -6,7 +6,7 @@ Pics.init = function(done) {
 	request.onreadystatechange = function() {
 		if (request.readyState === 4) {
 			if (request.status = 200 || request.status == 0) {
-				Pics.categories = JSON.parse(request.responseText);
+				Pics.collections = JSON.parse(request.responseText);
 				
 				request = new XMLHttpRequest();
 				request.onreadystatechange = function() {
@@ -15,12 +15,12 @@ Pics.init = function(done) {
 							var pictures = JSON.parse(request.responseText);
 							for (var i = 0; i < pictures.length; i++) {
 								var categories = pictures[i].collections.split(",");
-								for (var i2 = 0; i2 < Pics.categories.length; i2++) {
-									if (categories.includes(Pics.categories[i2].id)) {
-										if (!Pics.categories[i2].pics)
-											Pics.categories[i2].pics = [];
+								for (var i2 = 0; i2 < Pics.collections.length; i2++) {
+									if (categories.includes(Pics.collections[i2].id)) {
+										if (!Pics.collections[i2].pics)
+											Pics.collections[i2].pics = [];
 											
-										Pics.categories[i2].pics.push(pictures[i]);
+										Pics.collections[i2].pics.push(pictures[i]);
 									}
 								}
 							}
