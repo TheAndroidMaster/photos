33b7f21faea4c71b6214baa0f7c19494d51caaa3
James Fenn <18jafenn90@gmail.com>
2018-09-09 22:06:15 -0400

    add image titles

--- .scripts/update.js
+++ .scripts/update.js
@@ -54,6 +54,7 @@ try {
 
 			fs.writeFileSync(path.resolve("../../_images/" + image.id + ".md"), "---\n"
 				+ "layout: image\n"
+				+ "title: " + album.title + "\n"
 				+ "imgur: " + image.id + "\n"
 				+ "image: images/" + image.id + ".jpg\n"
 				+ (image.description ? "description: " + image.description.split(":").join("&#58;").split("-").join("&#8208;") + "\n" : "")
